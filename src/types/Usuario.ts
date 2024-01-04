ed4e8dab37fcf7378ac09585501a4758bc0f5bc5
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-04 14:50:20 -0300

    Criando arquivo de tipo para Usuário

---
+++ src/types/Usuario.ts
@@ -0,0 +1,47 @@
+enum Funcao {
+	ADMINISTRADOR,
+	GERENTE,
+	USUARIO,
+	INATIVO
+}
+
+interface IUsuario {
+	nome_completo: String,
+	nome_usuario: String,
+	email: String,
+	cpf: String,
+	numero_registro: String
+	senha: String,
+	imagem_url: String,
+	dados_administrativos: {
+		funcao: Funcao,
+		entidade_relacionada: String
+	}
+}
+
+class Usuario implements IUsuario {
+	nome_completo: String
+	nome_usuario: String
+	email: String
+	cpf: String
+	numero_registro: String
+	senha: String
+	imagem_url: String
+	dados_administrativos: { funcao: Funcao; entidade_relacionada: String }
+
+	constructor(usuario: Usuario){
+		const {cpf, dados_administrativos, email, imagem_url, nome_completo, nome_usuario, numero_registro, senha} = usuario
+
+		this.cpf = cpf
+		this.dados_administrativos = {
+			entidade_relacionada: dados_administrativos.entidade_relacionada,
+			funcao: dados_administrativos.funcao || Funcao.INATIVO
+		}
+		this.email = email
+		this.imagem_url = imagem_url
+		this.nome_completo = nome_completo
+		this.nome_usuario = nome_usuario
+		this.numero_registro = numero_registro
+		this.senha = senha
+	}
+}
