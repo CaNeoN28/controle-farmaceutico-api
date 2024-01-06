4a29a9b59cbbb9f20829ba81cc899237c557cae9
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-06 11:32:57 -0300

    Testando instancia do objeto de usuário

--- src/tests/models/Usuario.spec.ts
+++ src/tests/models/Usuario.spec.ts
@@ -1,3 +1,25 @@
-import Usuario from "../../app/models/Usuario"
+import mongoose from "mongoose";
+import Usuario from "../../app/models/Usuario";
 
-describe("O modelo de usuário", () => {})
+const dadosUsuario = {
+	_id: new mongoose.Types.ObjectId(),
+	cpf: "23841067085",
+	dados_administrativos: {
+		entidade_relacionada: new mongoose.Types.ObjectId(),
+		funcao: "ADMINISTRADOR"
+	},
+	email: "antoniobandeira@email.com",
+	imagem_url: ".jpg",
+	nome_completo: "Antonio Bandeira",
+	nome_usuario: "antonio_bandeira",
+	numero_registro: "0000000000",
+	senha: "12345678Asdf",
+};
+
+describe("O modelo de usuário", () => {
+	it("deve cadastrar um usuário com os dados informados", () => {
+		const usuario = new Usuario(dadosUsuario);
+
+		expect(usuario).toMatchObject(dadosUsuario)
+	});
+});
