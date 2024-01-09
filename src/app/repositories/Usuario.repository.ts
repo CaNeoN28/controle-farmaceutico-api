cbf8dd127b3a04d3e5e203ce8f4648fb55792617
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-09 14:44:10 -0300

    Criando a classe UsuarioRepository na metodologia de aplicação em camadas

---
+++ src/app/repositories/Usuario.repository.ts
@@ -0,0 +1,10 @@
+class UsuarioRepository {
+	static FindUsuarioId(id: any) {}
+	static FindUsuario(params: any) {}
+	static FindUsuarios(params: any) {}
+	static CreateUsuario(data: any) {}
+	static UpdateUsuario(id: string, data: any) {}
+	static DeleteUsuario(id: string) {}
+}
+
+export default UsuarioRepository
