d87d373859ef08487de9ddf148e23cb65b9e45ae
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-09 17:28:18 -0300

    Routas de imagem

---
+++ src/app/routes/imagemRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import ImagensControllers from "../controllers/ImagensController";
+
+const imagemRouter = Router();
+
+imagemRouter.post("/imagem", ImagensControllers.EnviarImagem);
+
+imagemRouter
+	.route("/imagem/:id")
+	.get(ImagensControllers.EncontrarImagemPorId)
+	.delete(ImagensControllers.RemoverImagem);
