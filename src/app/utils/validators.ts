8a1cddd64034bf1442a070e39326ab5ca7cc9f34
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-09 12:35:53 -0300

    Método para validação de nome de usuário

--- src/app/utils/validators.ts
+++ src/app/utils/validators.ts
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
 
-function validarID<T> (v: T) {
-	return mongoose.isValidObjectId(v)
+function validarID<T>(v: T) {
+	return mongoose.isValidObjectId(v);
 }
 
-function validarEmail (v: string) {
-	const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+function validarEmail(v: string) {
+	const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-	return regex.test(v)
+	return regex.test(v);
 }
 
-export {
-	validarID,
-	validarEmail
+function validarNomeDeUsuario(v: string) {
+	const regex = /^(?=.{3,}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/;
+
+	return regex.test(v);
 }
+
+export { validarID, validarEmail };
