b0ce23ea7bc85893de3dd915831d441b392d5fa8
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-09 12:30:39 -0300

    Método para checkar email

--- src/app/utils/validators.ts
+++ src/app/utils/validators.ts
@@ -4,6 +4,13 @@ function validarID<T> (v: T) {
 	return mongoose.isValidObjectId(v)
 }
 
+function validarEmail (v: string) {
+	const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+	return regex.test(v)
+}
+
 export {
-	validarID
+	validarID,
+	validarEmail
 }
