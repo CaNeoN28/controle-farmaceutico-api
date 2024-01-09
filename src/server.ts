4fcca29d51add5776ef159e021ff160a28a09805
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-09 17:12:37 -0300

    Permitindo o servidor de executar em uma porta

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
+import app from "./app/app";
 import ConnectDB from "./config/database_config";
-import * as dotenv from "dotenv"
+import * as dotenv from "dotenv";
 
 async function start() {
-	dotenv.config()
-	
-	await ConnectDB()
+	dotenv.config();
+	const { PORT } = process.env
+
+	await ConnectDB();
+
+	app.listen(PORT, () => {
+		console.log(`Servidor escutando em localhost:${PORT}`)
+	})
 }
 
-start()
+start();
