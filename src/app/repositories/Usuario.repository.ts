390b5f83a2ec4a44c195c30b4300503901a61dc8
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-09 19:46:42 -0300

    Tratando emails

--- src/app/repositories/Usuario.repository.ts
+++ src/app/repositories/Usuario.repository.ts
@@ -1,7 +1,35 @@
+import Erro from "../../types/Erro";
+import Usuario from "../../types/Usuario";
+import UsuarioModel from "../models/Usuario";
+
 class UsuarioRepository {
 	static findUsuarioId(id: any) {}
 	static findUsuarios(params: any) {}
-	static createUsuario(data: any) {}
+	static async createUsuario(data: Usuario) {
+		const usuario = new UsuarioModel(data);
+		let erro: Erro | undefined = undefined;
+
+		const primeiroUsuario = (await UsuarioModel.find()).length == 0;
+
+		if (primeiroUsuario) {
+			usuario.dados_administrativos.funcao = "ADMINISTRADOR";
+		}
+
+		const emailExiste = await UsuarioModel.findOne({ email: usuario.email });
+
+		if (emailExiste) {
+			erro = {
+				codigo: 409,
+				erro: "Email já cadastrado",
+			};
+		} else {
+			try {
+				await usuario.save();
+			} catch (error) {}
+		}
+
+		return { usuario, erro };
+	}
 	static updateUsuario(id: string, data: any) {}
 	static deleteUsuario(id: string) {}
 }
--- src/app/services/create.usuario.service.ts
+++ src/app/services/create.usuario.service.ts
@@ -1,3 +1,17 @@
-function createUsuarioService(data: any) {}
+import Usuario from "../../types/Usuario";
+import UsuarioRepository from "../repositories/Usuario.repository";
+
+async function createUsuarioService(data: Usuario) {
+	const {usuario, erro} = await UsuarioRepository.createUsuario(data)
+
+	if(erro){
+		throw {
+			codigo: erro.codigo,
+			erro: erro.erro
+		}
+	}
+
+	return usuario
+}
 
 export default createUsuarioService;
