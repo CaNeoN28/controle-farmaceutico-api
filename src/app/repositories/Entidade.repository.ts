9446ef8b6098523e076db35c9b90c186531a33f6
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-09 14:48:09 -0300

    Criando a classe eEntidadeRepository

---
+++ src/app/repositories/Entidade.repository.ts
@@ -0,0 +1,10 @@
+class EntidadeRepository {
+	static findUsuarioId(id: string) {}
+	static findUsuario(params: any) {}
+	static findUsuarios(params: any) {}
+	static createUsuario(data: any) {}
+	static updateUsuario(id: string, data: any) {}
+	static deleteUsuario(id: string) {}
+}
+
+export default EntidadeRepository;
