ab398687b640d0e54948e7a4de3c4e05545e5e22
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-10 12:27:04 -0300

    Teste para exibição de perfil

--- src/tests/rotas/autenticacao/perfil.spec.ts
+++ src/tests/rotas/autenticacao/perfil.spec.ts
@@ -1,18 +1,25 @@
+import request from "supertest";
 import UsuarioModel from "../../../app/models/Usuario";
 import { criarUsuarioAdm } from "../../../app/utils/gerarDadosDiversos";
 import { generateToken } from "../../../app/utils/jwt";
 import ILogin from "../../../types/ILogin";
+import app from "../../../app/app";
 
 let admin: ILogin = {
 	senha: "",
 	usuario: "",
 };
 
+let usuario: any = {};
+
 let token = "";
 
 beforeAll(async () => {
 	admin = await criarUsuarioAdm();
-	const usuario = await UsuarioModel.findOne({ nome_usuario: admin.usuario });
+	usuario = await UsuarioModel.findOne(
+		{ nome_usuario: admin.usuario },
+		{ senha: false }
+	);
 
 	if (usuario) {
 		token = generateToken({
@@ -29,4 +36,16 @@ afterAll(async () => {
 	await UsuarioModel.deleteMany();
 });
 
-describe("A rota de visualização de perfil deve", () => {});
+describe("A rota de visualização de perfil", () => {
+	it("deve retornar o perfil do usuário que estiver logado", async () => {
+		const resposta = await request(app)
+			.post("/perfil")
+			.set("Accept", "application/json")
+			.set("Authorization", `Bearer ${token}`)
+			.expect(200)
+			.then((res) => res.body);
+
+		expect(resposta).toMatch(usuario);
+		expect(resposta.senha).toBeUndefined()
+	});
+});
