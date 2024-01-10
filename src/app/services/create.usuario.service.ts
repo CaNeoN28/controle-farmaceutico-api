4cf5f7cf75cc6f8bfe3345870d3a674019d7669b
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-10 08:15:40 -0300

    Validando a senha

--- src/app/services/create.usuario.service.ts
+++ src/app/services/create.usuario.service.ts
@@ -1,17 +1,37 @@
 import Usuario from "../../types/Usuario";
 import UsuarioRepository from "../repositories/Usuario.repository";
+import bcrypt from "bcrypt";
+import { validarSenha } from "../utils/validators";
+import Erro from "../../types/Erro";
 
 async function createUsuarioService(data: Usuario) {
-	const {usuario, erro} = await UsuarioRepository.createUsuario(data)
+	const senha = data.senha;
 
-	if(erro){
-		throw {
-			codigo: erro.codigo,
-			erro: erro.erro
+	let { usuario, erro } = await UsuarioRepository.createUsuario(data);
+
+	if (senha) {
+		const senhaValida = validarSenha(senha as string);
+
+		if (!senhaValida) {
+			if (!erro) {
+				erro = {
+					codigo: 400,
+					erro: {},
+				};
+			}
+
+			erro.erro.senha = "Senha inválida"
 		}
 	}
 
-	return usuario
+	if (erro) {
+		throw {
+			codigo: erro.codigo,
+			erro: erro.erro,
+		};
+	}
+
+	return usuario;
 }
 
 export default createUsuarioService;
