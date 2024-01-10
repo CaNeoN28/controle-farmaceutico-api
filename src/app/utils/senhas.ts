0c55b8fdec11a3a1831eba4dfc62f232c162cb6b
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-10 09:32:09 -0300

    Adicionando método para comparar senha

--- src/app/utils/senhas.ts
+++ src/app/utils/senhas.ts
@@ -1,12 +1,17 @@
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 
-async function criptografarSenha(senha: string){
-	const salt = await bcrypt.genSalt(6)
-	const hash = await bcrypt.hash(senha, salt)
+async function criptografarSenha(senha: string) {
+	const salt = await bcrypt.genSalt(6);
+	const hash = await bcrypt.hash(senha, salt);
 
-	return hash
+	return hash;
 }
 
-export {
-	criptografarSenha
+async function compararSenha(senha: string, hash: string) {
+	return await bcrypt
+		.compare(senha, hash)
+		.then(() => true)
+		.catch(() => false);
 }
+
+export { criptografarSenha, compararSenha };
