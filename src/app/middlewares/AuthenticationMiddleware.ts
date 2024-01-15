3ddd03d356a39b36e946c63ac6aaf79760372105
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-15 13:09:12 -0300

    Testando se a autenticação é feita por um usuário válido

--- src/app/middlewares/AuthenticationMiddleware.ts
+++ src/app/middlewares/AuthenticationMiddleware.ts
@@ -1,18 +1,43 @@
 import { RequestHandler } from "express";
 import { AuthenticatedRequest } from "../../types/Requests";
-import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import TokenData from "../../types/TokenData";
+import findUsuarioService from "../services/find.usuario.service";
 
-const AuthenticationMiddleware: RequestHandler = function (
+const AuthenticationMiddleware: RequestHandler = async function (
 	req: AuthenticatedRequest,
 	res,
 	next
 ) {
+	const respostaErro = "É necessário estar autenticado para usar esta rota";
+
 	try {
-		req.user = {
-			id: new mongoose.Types.ObjectId(),
-		};
+		let token = req.headers["authorization"];
+
+		if (!token || !token.startsWith("Bearer ")) {
+			return res.status(401).send(respostaErro);
+		}
+
+		try {
+			token = token.split(" ")[1];
+
+			const decoded = jwt.verify(
+				token,
+				process.env.SECRET_KEY || ""
+			) as TokenData;
+
+			const usuario = await findUsuarioService(decoded.id);
+
+			if (!usuario) {
+				res.status(401).send(respostaErro);
+			}
+
+			req.user = decoded;
+		} catch {
+			res.status(401).send();
+		}
 	} catch {
-		return res.status(500).send();
+		return res.status(500).send(respostaErro);
 	}
 
 	next();
