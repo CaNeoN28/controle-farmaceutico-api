e2beb3a505107ed0aa2c2d86ab0438263e090086
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-16 11:05:09 -0300

    Criando base para o modelo de entidade

--- src/app/models/Entidade.ts
+++ src/app/models/Entidade.ts
@@ -1,6 +1,16 @@
 import mongoose from "mongoose";
 
-const EntidadeSchema = new mongoose.Schema({});
+const EntidadeSchema = new mongoose.Schema({
+	nome_entidade: {
+		type: String,
+	},
+	estado: {
+		type: String,
+	},
+	municipio: {
+		type: String,
+	},
+});
 
 const EntidadeModel = mongoose.model("Entidade", EntidadeSchema);
 
