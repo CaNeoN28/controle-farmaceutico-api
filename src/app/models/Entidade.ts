c9e55c599f6cd10f1eec8cd5f32f9512cb8965dc
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-16 11:10:59 -0300

    Criando propriedade obrigatória nos campos

--- src/app/models/Entidade.ts
+++ src/app/models/Entidade.ts
@@ -3,12 +3,15 @@ import mongoose from "mongoose";
 const EntidadeSchema = new mongoose.Schema({
 	nome_entidade: {
 		type: String,
+		required: [true, "Nome da entidade é obrigatório"],
 	},
 	estado: {
 		type: String,
+		required: [true, "Estado é obrigatório"],
 	},
 	municipio: {
 		type: String,
+		required: [true, "Município é obrigatório"],
 	},
 });
 
