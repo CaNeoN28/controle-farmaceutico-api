375447d8781fe0e0c71ce7310904dd42e9e520c5
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-16 10:25:23 -0300

    Começando arquivo de testes do modelo de entidade

--- src/app/utils/db/limparBanco.ts
+++ src/app/utils/db/limparBanco.ts
@@ -1,7 +1,9 @@
+import EntidadeModel from "../../models/Entidade";
 import UsuarioModel from "../../models/Usuario";
 
 async function limparBanco() {
 	await UsuarioModel.deleteMany();
+	await EntidadeModel.deleteMany()
 }
 
 export default limparBanco;
---
+++ src/tests/models/Entidade.spec.ts
@@ -0,0 +1,5 @@
+describe("O modelo de entidade", () => {
+	it("deve cadastrar uma entidade com os dados informados", () => {});
+	it("deve realizar validação dos atributos obrigatórios", () => {});
+	it("deve verificar atributos inválidos", () => {});
+});
