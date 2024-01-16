15056892d59a8e5f99d550ae1469c0d6c5f7c5f5
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-16 10:28:42 -0300

    Testando a criação de um objeto de entidade

--- src/tests/models/Entidade.spec.ts
+++ src/tests/models/Entidade.spec.ts
@@ -1,5 +1,17 @@
+import EntidadeModel from "../../app/models/Entidade";
+
+const dados = {
+	nome_entidade: "Ministério da Saúde",
+	estado: "Rondônia",
+	município: "Vilhena",
+};
+
 describe("O modelo de entidade", () => {
-	it("deve cadastrar uma entidade com os dados informados", () => {});
+	it("deve cadastrar uma entidade com os dados informados", () => {
+		const entidade = new EntidadeModel(dados);
+
+		expect(entidade).toMatchObject(dados);
+	});
 	it("deve realizar validação dos atributos obrigatórios", () => {});
 	it("deve verificar atributos inválidos", () => {});
 });
