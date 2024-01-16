bc0d96d03a174176264ebab2beb4b6f2a7dc470a
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-16 10:57:42 -0300

    Validando dados obrigatórios

--- src/tests/models/Entidade.spec.ts
+++ src/tests/models/Entidade.spec.ts
@@ -12,6 +12,44 @@ describe("O modelo de entidade", () => {
 
 		expect(entidade).toMatchObject(dados);
 	});
-	it("deve realizar validação dos atributos obrigatórios", () => {});
+	it("deve realizar validação dos atributos obrigatórios", async () => {
+		const entidade = new EntidadeModel({});
+
+		expect(entidade.validateSync).toThrow();
+
+		const validar = () => {
+			try {
+				entidade.validateSync();
+
+				return {};
+			} catch (error: any) {
+				const { nome_entidade, estado, municipio } = error.errors;
+
+				return {
+					nome_entidade: nome_entidade.message,
+					estado: estado.message,
+					municipio: municipio.message,
+				};
+			}
+		};
+
+		const erros = validar();
+
+		console.log(erros);
+
+		expect(erros).toHaveProperty(
+			"nome_entidade",
+			"Nome da entidade é obrigatório"
+		);
+		expect(erros).toHaveProperty(
+			"estado",
+			"Estado é obrigatório"
+		);
+		expect(erros).toHaveProperty(
+			"municipio",
+			"Município é obrigatório"
+		);
+	});
+
 	it("deve verificar atributos inválidos", () => {});
 });
