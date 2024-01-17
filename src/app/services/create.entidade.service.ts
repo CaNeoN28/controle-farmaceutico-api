42865eef9e656e9688bda50c7944080e2eeba0c7
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-17 09:07:04 -0300

    Criando serviço para cadastro de entidades

--- src/app/services/create.entidade.service.ts
+++ src/app/services/create.entidade.service.ts
@@ -1,3 +1,14 @@
-function createEntidadeService(data: any) {}
+import Entidade from "../../types/Entidade";
+import EntidadeRepository from "../repositories/Entidade.repository";
+
+async function createEntidadeService(data: Entidade) {
+	const { entidade, erro } = await EntidadeRepository.createEntidade(data);
+
+	if (erro) {
+		throw erro;
+	}
+
+	return entidade;
+}
 
 export default createEntidadeService;
