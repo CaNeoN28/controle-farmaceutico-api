298954c30b13e7afe4c87939383e2c41acc35a00
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-17 09:07:30 -0300

    Alterando o controller para cadastrar entidade'

--- src/app/controllers/EntidadesControllers.ts
+++ src/app/controllers/EntidadesControllers.ts
@@ -1,24 +1,44 @@
 import { RequestHandler } from "express";
+import Entidade from "../../types/Entidade";
+import createEntidadeService from "../services/create.entidade.service";
 
 class EntidadesControllers {
-	static EncontrarEntidadePorId: RequestHandler = async function (req, res, next) {
-		res.send("Recuperar entidade por Id")
-	}
+	static EncontrarEntidadePorId: RequestHandler = async function (
+		req,
+		res,
+		next
+	) {
+		res.send("Recuperar entidade por Id");
+	};
 
 	static ListarEntidades: RequestHandler = async function (req, res, next) {
-		res.send("Listar entidades")
-	}
+		res.send("Listar entidades");
+	};
 
 	static CriarEntidade: RequestHandler = async function (req, res, next) {
-		res.send("Criar entidade")
-	}
+		const { estado, municipio, nome_entidade }: Entidade = req.body;
+
+		const data = {
+			estado,
+			municipio,
+			nome_entidade,
+		};
+
+		try {
+			const entidade = await createEntidadeService(data);
+
+			return res.status(201).send(entidade);
+		} catch (error: any) {
+			next(error);
+		}
+	};
 
 	static AtualizarEntidade: RequestHandler = async function (req, res, next) {
-		res.send("Atualizer entidade")
-	}
+		res.send("Atualizer entidade");
+	};
 
 	static RemoverEntidade: RequestHandler = async function (req, res, next) {
-		res.send("Remover entidade")
-	}
+		res.send("Remover entidade");
+	};
 }
 export default EntidadesControllers;
