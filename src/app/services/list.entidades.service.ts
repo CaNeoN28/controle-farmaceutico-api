4bb0f5bab8a1b746514f8f2344f8e3a5e27c4278
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-18 09:21:08 -0300

    Criando service para listagem de entidades

--- src/app/services/list.entidades.service.ts
+++ src/app/services/list.entidades.service.ts
@@ -1,3 +1,24 @@
-function listEntidadesService(params: any) {}
+import { FiltrosEntidade } from "../../types/Entidade";
+import EntidadeRepository from "../repositories/Entidade.repository";
+
+async function listEntidadesService(params: FiltrosEntidade) {
+	const { estado, municipio, nome_entidade } = params;
+	const filtros: FiltrosEntidade = {};
+
+	if (estado) {
+		filtros.estado = estado;
+	}
+	if (municipio) {
+		filtros.municipio = municipio;
+	}
+	if (nome_entidade) {
+		filtros.nome_entidade = new RegExp(nome_entidade);
+	}
+
+	const entidades = await EntidadeRepository.findEntidades(filtros)
+
+	return entidades
+
+}
 
 export default listEntidadesService;
