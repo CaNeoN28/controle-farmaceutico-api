e3b87501b72d0a04a737fcd50976b28c62014da0
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-20 10:45:15 -0300

    Testando a validação do modelo

--- src/tests/models/Farmacia.spec.ts
+++ src/tests/models/Farmacia.spec.ts
@@ -20,14 +20,73 @@ const dados = new Farmacia({
 
 describe("O modelo de farmácia", () => {
 	it("deve criar com sucesso uma farmácia válida", () => {
-		const farmacia = new FarmaciaModel(dados)
-		const erros = farmacia.validateSync()
+		const farmacia = new FarmaciaModel(dados);
+		const erros = farmacia.validateSync();
 
-		expect(erros).toBeUndefined()
+		expect(erros).toBeUndefined();
 		expect(farmacia).toMatchObject({
 			...farmacia,
 			plantoes: [],
-			horarios_servico: []
-		})
-	})
+			horarios_servico: [],
+		});
+	});
+
+	it("deve realizar validação dos dados de farmácia", () => {
+		const farmacia = new FarmaciaModel({
+			cnpj: "00000000000000",
+			endereco: {
+				bairro: "BI",
+				cep: "00000000",
+				estado: "Estado inválido",
+				localizacao: {},
+				logradouro: "LI",
+				municipio: "Município inválido",
+				numero: "NI",
+			},
+			nome_fantasia: "NF",
+		});
+
+		const validar = () => {
+			const erros = farmacia.validateSync()!;
+			const {
+				cnpj,
+				nome_fantasia: nome_fantasia,
+				"endereco.bairro": endereco_bairro,
+				"endereco.cep": endereco_cep,
+				"endereco.estado": endereco_estado,
+				"endereco.localizacao": endereco_localizacao,
+				"endereco.logradouro": endereco_logradouro,
+				"endereco.municipio": endereco_municipio,
+				"endereco.numero": endereco_numero,
+			} = erros.errors;
+
+			return {
+				cnpj: cnpj.message,
+				nome_fantasia: nome_fantasia.message,
+				endereco_bairro: endereco_bairro.message,
+				endereco_cep: endereco_cep.message,
+				endereco_estado: endereco_estado.message,
+				endereco_localizacao: endereco_localizacao.message,
+				endereco_logradouro: endereco_logradouro.message,
+				endereco_municipio: endereco_municipio.message,
+				endereco_numero: endereco_numero.message,
+			};
+		};
+
+		expect(validar).not.toThrow();
+
+		const erros = validar();
+
+		expect(erros).toMatchObject({
+			cnpj: "CNPJ inválido",
+			nome_fantasia: "Nome fantasia inválido",
+			endereco_bairro: "Bairro inválido",
+			endereco_cep: "CEP inválido",
+			endereco_estado: "Estado inválido",
+			endereco_municipio: "Município inválido",
+			endereco_localizacao: "Localizacao inválida",
+			endereco_logradouro: "Logradouro inválido",
+			endereco_numero: "Número inválido"
+		});
+	});
 });
