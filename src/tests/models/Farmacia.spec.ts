7a5816b897a05ff6df8008b603bcb4c939dbac0c
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-20 10:46:24 -0300

    Testando a validação de atributos obrigatórios

--- src/tests/models/Farmacia.spec.ts
+++ src/tests/models/Farmacia.spec.ts
@@ -31,6 +31,53 @@ describe("O modelo de farmácia", () => {
 		});
 	});
 
+	it("deve realizar validação dos atributos obrigatórios", () => {
+		const farmacia = new FarmaciaModel({});
+
+		const validar = () => {
+			const erros = farmacia.validateSync()!;
+			const {
+				cnpj,
+				nome_fantasia: nome_fantasia,
+				"endereco.bairro": endereco_bairro,
+				"endereco.cep": endereco_cep,
+				"endereco.estado": endereco_estado,
+				"endereco.localizacao": endereco_localizacao,
+				"endereco.logradouro": endereco_logradouro,
+				"endereco.municipio": endereco_municipio,
+				"endereco.numero": endereco_numero,
+			} = erros.errors;
+
+			return {
+				cnpj: cnpj.message,
+				nome_fantasia: nome_fantasia.message,
+				endereco_bairro: endereco_bairro.message,
+				endereco_cep: endereco_cep.message,
+				endereco_estado: endereco_estado.message,
+				endereco_localizacao: endereco_localizacao.message,
+				endereco_logradouro: endereco_logradouro.message,
+				endereco_municipio: endereco_municipio.message,
+				endereco_numero: endereco_numero.message,
+			};
+		};
+
+		expect(validar).not.toThrow();
+
+		const erros = validar();
+
+		expect(erros).toMatchObject({
+			cnpj: "CNPJ é obrigatório",
+			nome_fantasia: "Nome fantasia é obrigatório",
+			endereco_bairro: "Bairro é obrigatório",
+			endereco_cep: "CEP é obrigatório",
+			endereco_estado: "Estado é obrigatório",
+			endereco_municipio: "Município é obrigatório",
+			endereco_localizacao: "Localizacao é obrigatório",
+			endereco_logradouro: "Logradouro é obrigatório",
+			endereco_numero: "Número é obrigatório",
+		});
+	});
+
 	it("deve realizar validação dos dados de farmácia", () => {
 		const farmacia = new FarmaciaModel({
 			cnpj: "00000000000000",
@@ -86,7 +133,7 @@ describe("O modelo de farmácia", () => {
 			endereco_municipio: "Município inválido",
 			endereco_localizacao: "Localizacao inválida",
 			endereco_logradouro: "Logradouro inválido",
-			endereco_numero: "Número inválido"
+			endereco_numero: "Número inválido",
 		});
 	});
 });
