8cd3bd0dc8f165a484b03fc98f1f9fdf879dc88c
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-22 16:21:08 -0300

    Configurando base para o service de atualização de farmácias

--- src/app/services/update.farmacia.service.ts
+++ src/app/services/update.farmacia.service.ts
@@ -1,7 +1,15 @@
+import FarmaciaRepository from "../repositories/Farmacia.repository";
 import { validarID } from "../utils/validators";
 
-function updateFarmaciaService(id: string, data: any) {
+async function updateFarmaciaService(id: string, data: any) {
 	if (validarID<string>(id)) {
+		const {erros, farmacia} = await FarmaciaRepository.updateFarmacia(id, data)
+
+		if(erros){
+			throw erros
+		}
+
+		return farmacia
 	} else {
 		throw {
 			codigo: 400,
