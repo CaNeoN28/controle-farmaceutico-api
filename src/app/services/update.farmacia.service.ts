1152d738754dea92948130651157ad4cbb53c59e
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-22 16:30:32 -0300

    Validando horário de serviço na atualização de farmácia

--- src/app/services/update.farmacia.service.ts
+++ src/app/services/update.farmacia.service.ts
@@ -1,15 +1,48 @@
+import Erro from "../../types/Erro";
 import FarmaciaRepository from "../repositories/Farmacia.repository";
+import validarDiasServico from "../utils/validarHorarioServico";
 import { validarID } from "../utils/validators";
 
 async function updateFarmaciaService(id: string, data: any) {
 	if (validarID<string>(id)) {
-		const {erros, farmacia} = await FarmaciaRepository.updateFarmacia(id, data)
+		const { horarios_servico } = data;
+		let erro: Erro | undefined = undefined;
 
-		if(erros){
-			throw erros
+		if (horarios_servico) {
+			const errosDiasDeServico = validarDiasServico(horarios_servico);
+
+			if (errosDiasDeServico) {
+				erro = {
+					codigo: 400,
+					erro: errosDiasDeServico,
+				};
+			}
 		}
 
-		return farmacia
+		const { erros, farmacia } = await FarmaciaRepository.updateFarmacia(
+			id,
+			data
+		);
+
+		if(erros) {
+			if (erro) {
+				erro = {
+					codigo: erros.codigo,
+					erro: {
+						...erros.erro,
+						...erro.erro,
+					},
+				};
+			} else {
+				erro = erros;
+			}
+		}
+
+		if (erro) {
+			throw erro;
+		}
+
+		return farmacia;
 	} else {
 		throw {
 			codigo: 400,
