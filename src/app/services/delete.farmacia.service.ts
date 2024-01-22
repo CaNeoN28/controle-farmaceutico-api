dad38acaf40156d497a324101e8a07483db070b6
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-22 16:37:25 -0300

    Criando base para o service de remoção de farmácia

--- src/app/services/delete.farmacia.service.ts
+++ src/app/services/delete.farmacia.service.ts
@@ -1,3 +1,11 @@
-function deleteFarmaciaService(id: string) {}
+import FarmaciaRepository from "../repositories/Farmacia.repository";
+
+async function deleteFarmaciaService(id: string) {
+	const { farmacia, erro } = await FarmaciaRepository.deleteFarmacia(id);
+
+	if (erro) {
+		throw erro;
+	}
+}
 
 export default deleteFarmaciaService;
