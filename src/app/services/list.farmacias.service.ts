59a7648c25da9fe9c4a997c699b43d2d7d5145ac
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-22 16:12:55 -0300

    Adicionando filtros à listagem de farmácias

--- src/app/services/list.farmacias.service.ts
+++ src/app/services/list.farmacias.service.ts
@@ -1,20 +1,40 @@
 import FarmaciaRepository from "../repositories/Farmacia.repository";
 import { extrairPaginacao } from "../utils/paginacao";
 
-interface FiltrosFarmacia {}
+interface FiltrosFarmacia {
+	nome_fantasia: string;
+	municipio: string;
+	bairro: string;
+	estado: string;
+}
 
 async function listFarmaciasService(params: any) {
-	const filtros = {};
+	const { bairro, estado, municipio, nome_fantasia }: FiltrosFarmacia = params;
+	const filtros: any = {};
+
+	if (bairro) {
+		filtros.bairro = new RegExp(bairro, "i");
+	}
+	if (estado) {
+		filtros.estado = new RegExp(estado, "i");
+	}
+	if (municipio) {
+		filtros.municipio = new RegExp(municipio, "i");
+	}
+	if (nome_fantasia) {
+		filtros.nome_fantasia = new RegExp(nome_fantasia, "i");
+	}
+
 	const { limite, pagina } = extrairPaginacao(params);
 
 	const paginacao = {
 		limite: limite || 10,
-		pagina: pagina || 1
-	}
+		pagina: pagina || 1,
+	};
 
-	const farmacias = await FarmaciaRepository.findFarmacias(filtros, paginacao)
+	const farmacias = await FarmaciaRepository.findFarmacias(filtros, paginacao);
 
-	return farmacias
+	return farmacias;
 }
 
 export default listFarmaciasService;
