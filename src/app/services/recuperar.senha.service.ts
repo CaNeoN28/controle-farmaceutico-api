58f769c5ffb8ed5fa08b5fb7e0d8b1e322fe4a57
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-23 18:01:51 -0300

    Corrigindo erro na mensagem de erro

--- src/app/services/recuperar.senha.service.ts
+++ src/app/services/recuperar.senha.service.ts
@@ -16,10 +16,10 @@ async function recuperarSenhaService(email: string | undefined) {
 		if(usuario.dados_administrativos.funcao == "INATIVO"){
 			throw {
 				codigo: 403,
-				erro: "O usuário ainda está inativo, espera sua ativação"
+				erro: "O usuário ainda está inativo, espere sua ativação"
 			}
 		}
-		
+
 		const { email, nome_usuario } = usuario;
 		const expiraEm = 30 * 60;
 		const token = generateToken({ email, nome_usuario }, expiraEm);
