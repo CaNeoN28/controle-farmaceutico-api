1507634902c10fd202b592ec31c224df0d64360a
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-23 10:11:55 -0300

    Recebendo imagem

--- src/app/controllers/ImagensController.ts
+++ src/app/controllers/ImagensController.ts
@@ -1,12 +1,31 @@
 import { RequestHandler } from "express";
 
 class ImagensControllers {
-	static EncontrarImagemPorId: RequestHandler = async function (req, res, next) {
+	static EncontrarImagemPorId: RequestHandler = async function (
+		req,
+		res,
+		next
+	) {
 		res.send("Encontrar imagem por ID");
 	};
 
 	static EnviarImagem: RequestHandler = async function (req, res, next) {
-		res.send("Enviar imagem");
+		try {
+			const arquivo = req.files;
+
+			if (arquivo) {
+				const imagem = arquivo.imagem;
+			} else {
+				throw {
+					codigo: 400,
+					erro: "É necessário o envio de um arquivo",
+				};
+			}
+
+			res.status(201).send("Enviado");
+		} catch (error) {
+			next(error);
+		}
 	};
 
 	static RemoverImagem: RequestHandler = async function (req, res, next) {
