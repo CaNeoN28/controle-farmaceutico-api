73226f4280af8bbb30677e10d3524738a071c1a4
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-24 13:22:29 -0300

    Listando farmácias

--- src/app/services/list.escala.farmacia.service.ts
+++ src/app/services/list.escala.farmacia.service.ts
@@ -1,11 +1,31 @@
-interface Filtros {
+import FarmaciaRepository from "../repositories/Farmacia.repository";
+
+interface Parametros {
 	municipio?: string;
 	estado?: string;
 	tempo?: string;
 }
 
-async function listPorEscalaFarmaciaService(params: Filtros) {
-	
+async function listPorEscalaFarmaciaService(params: Parametros) {
+	const { estado, municipio, tempo } = params;
+	const filtros: any = {};
+
+	if (estado) {
+		filtros.estado = estado;
+	}
+
+	if (municipio) {
+		filtros.municipio = municipio;
+	}
+
+	const paginacao = {
+		pagina: 1,
+		limite: 1000,
+	};
+
+	const {dados: farmacias} = await FarmaciaRepository.findFarmacias(filtros, paginacao);
+
+	return farmacias;
 }
 
 export default listPorEscalaFarmaciaService;
