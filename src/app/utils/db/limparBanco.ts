eec1d1fc7cc1eedca95b0b9f36b8c79e7b6fc59a
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-24 08:49:55 -0300

    Adicionando método para limpar banco ao package.json

--- src/app/utils/db/limparBanco.ts
+++ src/app/utils/db/limparBanco.ts
@@ -1,3 +1,4 @@
+import ConnectDB from "../../../config/database_config";
 import EntidadeModel from "../../models/Entidade";
 import FarmaciaModel from "../../models/Farmacia";
 import UsuarioModel from "../../models/Usuario";
@@ -6,6 +7,15 @@ async function limparBanco() {
 	await UsuarioModel.deleteMany();
 	await EntidadeModel.deleteMany();
 	await FarmaciaModel.deleteMany();
+}
+
+try {
+	ConnectDB();
+	limparBanco().then((res) => {
+		console.log("Banco limpo com sucesso");
+	});
+} catch (error) {
+	console.log(error);
 }
 
 export default limparBanco;
