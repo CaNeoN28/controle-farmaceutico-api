73cc78ef2d81d9a0c7038bb28fcc6b3959c7827f
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-24 11:13:59 -0300

    Convertendo dia para coincidir com o plantão das 7 as 7

--- src/app/utils/farmaciasAbertas.ts
+++ src/app/utils/farmaciasAbertas.ts
@@ -19,20 +19,65 @@ function encontrarDiaSemana(tempo: string, horarios_servico: HorariosServico) {
 	return horario_servico;
 }
 
+function encontrarPlantao(tempo: string, plantoes?: string[]) {
+	if (!plantoes) return false;
+
+	const plantao = plantoes.find((p) => {
+		const plantao = new Date(p).getTime();
+		const diaProximo = () => {
+			const dateTime = new Date(tempo);
+
+			const hora = dateTime.getHours();
+			let { dia, mes, ano } = {
+				dia: dateTime.getDate(),
+				mes: dateTime.getMonth() + 1,
+				ano: dateTime.getFullYear(),
+			};
+
+			if (hora < 7) {
+				if (dia == 1) {
+					mes--;
+
+					if (mes == 0) {
+						ano--;
+						mes = 12;
+					}
+					if (mes == 2) {
+						dia = ano % 4 == 0 ? 29 : 28;
+					} else if ([1, 3, 5, 7, 8, 10, 12].includes(mes)) {
+						dia = 31;
+					} else {
+						dia = 30;
+					}
+				} else {
+					dia--;
+				}
+			}
+			const data = new Date([ano, mes, dia].join("/"));
+			console.log(data);
+			return data.getTime();
+		};
+
+		return diaProximo() === plantao;
+	});
+
+	return plantao;
+}
+
 function farmaciasAbertas(
 	farmacias: (Farmacia & { id: string })[],
 	tempo: string
 ) {
 	farmacias = farmacias.filter((f) => {
 		const diaSemana = encontrarDiaSemana(tempo, f.horarios_servico);
-		if (!diaSemana) {
+		const plantao = encontrarPlantao(tempo, f.plantoes);
+		if (!diaSemana && !plantao) {
 			return false;
 		}
 
 		return true;
 	});
-	
-	console.log(farmacias)
+	console.log(farmacias);
 	return farmacias;
 }
 
