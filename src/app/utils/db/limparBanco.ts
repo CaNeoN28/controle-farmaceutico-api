1767861ffd1084b3ccac722be0a8785e08bd2c62
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-24 08:55:21 -0300

    Movendo para um método a parte

--- src/app/utils/db/limparBanco.ts
+++ src/app/utils/db/limparBanco.ts
@@ -1,4 +1,3 @@
-import ConnectDB from "../../../config/database_config";
 import EntidadeModel from "../../models/Entidade";
 import FarmaciaModel from "../../models/Farmacia";
 import UsuarioModel from "../../models/Usuario";
@@ -7,15 +6,6 @@ async function limparBanco() {
 	await UsuarioModel.deleteMany();
 	await EntidadeModel.deleteMany();
 	await FarmaciaModel.deleteMany();
-}
-
-try {
-	ConnectDB();
-	limparBanco().then((res) => {
-		console.log("Banco limpo com sucesso");
-	});
-} catch (error) {
-	console.log(error);
 }
 
 export default limparBanco;
