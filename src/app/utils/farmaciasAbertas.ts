b7a0be5c97f9d055e3986f12f2464ec4ce75e669
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-24 10:45:17 -0300

    Filtrando por dia da semana

--- src/app/utils/farmaciasAbertas.ts
+++ src/app/utils/farmaciasAbertas.ts
@@ -1,9 +1,38 @@
-import Farmacia from "../../types/Farmacia";
+import Farmacia, { HorariosServico } from "../../types/Farmacia";
+
+function encontrarDiaSemana(tempo: string, horarios_servico: HorariosServico) {
+	const dias_semana = [
+		"domingo",
+		"segunda_feira",
+		"terca_feira",
+		"quarta_feira",
+		"quinta_feira",
+		"sexta_feira",
+		"sabado",
+	];
+
+	const dia = new Date(tempo).getDay();
+	const dia_semana = dias_semana[dia];
+
+	const horario_servico = horarios_servico[dia_semana as keyof HorariosServico];
+
+	return horario_servico;
+}
 
 function farmaciasAbertas(
 	farmacias: (Farmacia & { id: string })[],
 	tempo: string
 ) {
+	farmacias = farmacias.filter((f) => {
+		const diaSemana = encontrarDiaSemana(tempo, f.horarios_servico);
+		if (!diaSemana) {
+			return false;
+		}
+
+		return true;
+	});
+	
+	console.log(farmacias)
 	return farmacias;
 }
 
