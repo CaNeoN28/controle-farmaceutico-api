46ad5e575a4a5d601744e0aa0aade6bbbf638a80
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-01-26 15:24:22 -0300

    Criando base para a documentação das rotas de farmácia

--- src/docs/paths/farmacias.ts
+++ src/docs/paths/farmacias.ts
@@ -1,5 +1,29 @@
 import { Paths } from "swagger-jsdoc";
 
-const FarmaciasPaths: Paths = {};
+const FarmaciasPaths: Paths = {
+	"/farmacia": {
+		post: {},
+	},
+	"/farmacia/{id}": {
+		parameters: {
+			name: "id",
+			in: "path",
+			required: true,
+			description: "ID da farmácia",
+		},
+		get: {},
+		put: {},
+		delete: {},
+	},
+	"/farmacias": {
+		get: {},
+	},
+	"/farmacia/proxima": {
+		get: {},
+	},
+	"/farmacias/plantao": {
+		get: {},
+	},
+};
 
 export default FarmaciasPaths;
