fcc46446ff5675228c1ff0d42509a3d5baf4daaf
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-03-02 17:26:06 -0300

    Alterando a documentação

--- src/docs/paths/imagens.ts
+++ src/docs/paths/imagens.ts
@@ -13,12 +13,40 @@ const ParametrosId = [
 ];
 
 const ImagensPaths: Paths = {
-	"/imagem": {
+	"/imagem/{id}": {
+		get: {
+			tags: ["Imagens"],
+			summary: "Recupera uma imagem da API",
+			description:
+				"Retorna uma imagem enviada à API. Seu nome funciona como ID, nas não é a mesma coisa.",
+			security: [],
+			parameters: ParametrosId,
+			responses: {
+				200: {
+					description: "Retorna a imagem com sucesso",
+					content: {
+						"image/png": {},
+						"image/jpg": {},
+						"image/jpeg": {},
+					},
+				},
+				404: {
+					description: "Retorna erro se a imagem não for encontrada",
+					content: {
+						"text/html": {},
+					},
+				},
+				500: {
+					$ref: "#/components/responses/ErroInterno",
+				},
+			},
+		},
+	},
+	"/{finalidade}/imagem": {
 		post: {
 			tags: ["Imagens"],
 			summary: "Envia uma imagem",
-			description:
-				"Envia várias imagens e retorna seus caminhos na API. É preciso estar autenticado.",
+			description: "Envia várias imagens e retorna seus caminhos na API.",
 			security: [{ BearerAuth: [] }],
 			requestBody: {
 				content: {
@@ -66,28 +94,51 @@ const ImagensPaths: Paths = {
 			},
 		},
 	},
-	"/imagem/{id}": {
-		get: {
+	"/{finalidade}/{id_finalidade}/imagem/{caminho}": {
+		put: {
 			tags: ["Imagens"],
-			summary: "Recupera uma imagem da API",
-			description:
-				"Retorna uma imagem enviada à API. Seu nome funciona como ID, nas não é a mesma coisa.",
-			security: [],
-			parameters: ParametrosId,
-			responses: {
-				200: {
-					description: "Retorna a imagem com sucesso",
-					content: {
-						"image/png": {},
-						"image/jpg": {},
-						"image/jpeg": {},
+			summary: "Confirma o envio de uma imagem",
+			description: "Envia uma única imagem confirma seu salvanto na API.",
+			security: [{ BearerAuth: [] }],
+			requestBody: {
+				content: {
+					"multipart/form-data": {
+						schema: {
+							type: "object",
+							properties: {
+								imagem: {
+									type: "array",
+									items: {
+										type: "string",
+										format: "binary",
+									},
+								},
+							},
+						},
 					},
 				},
-				404: {
-					description: "Retorna erro se a imagem não for encontrada",
+			},
+			responses: {
+				201: {
+					description: "Salva as imagens e retorna seu endereço na API",
 					content: {
-						"text/html": {},
+						"application/json": {
+							schema: {
+								type: "object",
+								properties: {
+									nome_da_imagem: {
+										type: "string",
+									},
+								},
+							},
+						},
 					},
+				},
+				400: {
+					description: "Retorna erros se não for possível salvar imagem",
+				},
+				401: {
+					$ref: "#/components/responses/ErroAutenticacao",
 				},
 				500: {
 					$ref: "#/components/responses/ErroInterno",
