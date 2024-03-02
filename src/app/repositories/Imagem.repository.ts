f52243c75e2b42c97d985fe66f40aa22df3f34ec
Carlos Felipe Steinheuser <carlosfelipe.st28@gmail.com>
2024-03-02 12:18:23 -0300

    Criando services para imagem

---
+++ src/app/repositories/Imagem.repository.ts
@@ -0,0 +1,14 @@
+export default class ImagemRepository {
+	static async criarImagem(finalidade: string) {}
+	static async confirmarImagem(
+		finalidade: string,
+		id_finalidade: string,
+		caminho: string
+	) {}
+
+	static async removerImagem(
+		finalidade: string,
+		id_finalidade: string,
+		caminho: string
+	) {}
+}
